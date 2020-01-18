5c679ade16d0eb4124a7e7e2049dfbb294b7ebac
Daniel Pombo <pombodaniel10@gmail.com>
2020-01-18 17:13:01 -0500

    minor changes

--- server/config/express.js
+++ server/config/express.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const tasks = require("../routes/tasks");
 const users = require("../routes/users");
 const app = express();
+const compression = require('compression');
+const helmet = require('helmet');
 
 var distDir = '../../public/';
 
@@ -15,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(helmet());
+app.use(compression()); 
 
 app.use('/tasks', tasks);
 
