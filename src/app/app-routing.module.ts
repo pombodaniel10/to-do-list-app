0c7863fa728727ac7b120815176d280ca2396b4e
Daniel Pombo <pombodaniel10@gmail.com>
2020-01-18 10:18:45 -0500

    update routes and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {AddTaskComponent} from './add-task/add-task.component';
+import {TasksListComponent} from './tasks-list/tasks-list.component';
+import {UsersListComponent} from './users-list/users-list.component';
+import {EditTaskComponent} from './edit-task/edit-task.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'add-task',component:AddTaskComponent},
+  {path:'tasks-list',component:TasksListComponent},
+  {path:'users-list',component:UsersListComponent},
+  {path:'edit-task/:id',component:EditTaskComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule, MatFormFieldModule } from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent,SelectUserDialog } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TasksListComponent,DeleteDialog } from './tasks-list/tasks-list.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { UsersListComponent,AddUserDialog } from './users-list/users-list.component';
+
+import {ApiService} from './services/api.service';
+import { TaskComponent } from './task/task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TasksListComponent,
+    AddTaskComponent,
+    UsersListComponent,
+    SelectUserDialog,
+    DeleteDialog,
+    AddUserDialog,
+    TaskComponent,
+    EditTaskComponent
   ],
+  entryComponents: [SelectUserDialog,DeleteDialog,AddUserDialog],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [ApiService,{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
